c651dff8123fb6f674fa71f8319fbaa4c48f92b9
Vhiz <marllonsantosnascimento@gmail.com>
2025-09-03 19:47:05 -0300

    Atualizando regras - Impedindo inserções vazias

--- src/com/Vhiz/CRUD2/MainAPP.java
+++ src/com/Vhiz/CRUD2/MainAPP.java
@@ -7,7 +7,7 @@ public class MainAPP {
 		Scanner sc = new Scanner(System.in);
 		AlunoService alunoService = new AlunoService();
 		int opcao = -1, idade = 0;
-		String matBusca, curso, matricula, nome;
+		String matBusca, curso = null, matricula = null, nome;
 		
 		
 		do {
@@ -29,10 +29,15 @@ public class MainAPP {
             }
             switch (opcao) {
             
-            
+            //Adiciona alunos à lista
             case 1:
+            	do {
             	System.out.println("Nome: ");
             	nome = sc.nextLine();
+            	nome = nome.replaceAll("\\s", "");
+            	if (nome.isEmpty())
+            		System.out.println("O nome está vazio! Insira um nome válido!");
+            	} while (nome.isEmpty());
             	
             	
             	System.out.println("Idade: ");
@@ -46,21 +51,34 @@ public class MainAPP {
             	}
             	
             	
+            	do {
             	System.out.println("Curso: ");
             	curso = sc.nextLine();
+            	curso = curso.replaceAll("\\s", "");
+            	if (curso.isEmpty())
+            		System.out.println("O curso está vazio! Insira um curso válido!");
+            	} while (curso.isEmpty());
             	
+            	
+            	do {
             	System.out.println("Matricula: ");
             	matricula = sc.nextLine();
+            	matricula = matricula.replaceAll("\\s", "");
+            	if (matricula.isEmpty())
+            		System.out.println("A matricula está vazia! Insira uma matricula válida!");
+            	} while (matricula.isEmpty());
             	
             	Aluno novoAluno = new Aluno(nome, idade, curso, matricula);
             	alunoService.adicionarAluno(matricula, novoAluno);
             	
             	break;
-            	
+            
+            //Lista todos os alunos
             case 2:
             	alunoService.listarAlunos();
             	break;
             	
+            //Busca aluno por matrícula e exibe o resultado.
             case 3:
             	System.out.println("Digite a matricula: ");
             	matBusca = sc.nextLine();
@@ -70,7 +88,8 @@ public class MainAPP {
             				() -> System.out.println("Aluno não encontrado")
             				);
             	break;
-            	
+            
+            //Atualiza o aluno da matricula escolhida.
             case 4:
             	System.out.println("Digite a Matricula do aluno a atualizar: ");
             	matBusca = sc.nextLine();
@@ -95,12 +114,14 @@ public class MainAPP {
             	System.out.println(alunoService.buscarPorMatricula(matBusca));
             	break;
             	
+            //Exclui o aluno através do numero de matricula passado.
             case 5:
             	System.out.println("Digite a Matricula do aluno a ser deletado:");
             	matBusca = sc.nextLine();
             	alunoService.removerAluno(matBusca);
             	break;
             	
+            //Finaliza o sistema.
             case 0:
             	System.out.println("O Sistema está fechando agora!");
             	break;
